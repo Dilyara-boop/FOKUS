2dc163ddcf9f8fd5f06c55b59e4e481ccc10af30
Dilyara-boop <tatstroydom@yandex.ru>
2026-09-21 11:11:31 +0300

    Обновление кэша приложения

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "focus-v1";
+const CACHE = "focus-v4";
 const LOCAL_ASSETS = [
     "./", "index.html", "style.css", "cards.css", "money.css", "lessons.css", "progress.css",
     "script.js", "cards.js", "money.js", "lessons.js", "progress.js", "companion.js", "sync.js", "pwa.js",
@@ -6,6 +6,10 @@ const LOCAL_ASSETS = [
     "images/character.png", "images/english.png", "images/myself.png", "images/stuck.png", "images/tatstroydom.png",
     "images/cards/01-path.png", "images/cards/02-choice.png", "images/cards/03-sea.png", "images/cards/04-shelter.png",
     "images/cards/05-window.png", "images/cards/06-garden.png", "images/cards/07-bridge.png"
+    , "images/cards/08-boat.webp", "images/cards/09-road.webp", "images/cards/10-door.webp"
+    , "images/cards/11-bridge.webp", "images/cards/12-forest-light.webp", "images/cards/13-stairs.webp"
+    , "images/cards/14-tree.webp", "images/cards/15-sea-window.webp", "images/cards/16-rock-path.webp"
+    , "images/cards/17-two-gates.webp"
 ];
 
 self.addEventListener("install", event => {
